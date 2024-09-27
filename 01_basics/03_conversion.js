f20fac31eacbcaa3f095579a3939fbcdb6ecc069
Shivkumar-7 <136238960+Shivkumar-7@users.noreply.github.com>
2024-09-27 19:09:39 +0000

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -24,3 +24,65 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "shiv"
+let str2 = "kumar"
+console.log(str1 + str2)
+
+console.log("1"+ 2);
+console.log("2" + 1);
+console.log("2" + "1");
+console.log("1" + 2 + 2);
+console.log(1 + 1 + "2");
+
+console.log(true);
+console.log(+true);
+console.log("");
+console.log(null);
+console.log(+"");
+
+console.log("2" > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//equality check == and comparisons < > >= <= work differently , comparisons convert null to 0.
+
+// Primitive data Types {call by value}
+// 7 ->String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Reference type (Non primitive)
+//Array, Objects, Functions
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id==anotherId);
+
+const heros = ["shaktiman", "nagraj", "doga"];
+let obj = {
+    name: "shiv",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("karo");
+}
+
+//stack (primitive data type) call by value
+
+//Heap (Non primitive data type) call by reference
+
+let userOne = {
+    email: "user.google",
+    upi: "aa",
+}
+
+let userTwo = userOne
+userTwo.email = "reus.google"
+
+console.table(userOne);
+
